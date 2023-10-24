0f05d347d29dfb3f0da45699760aa30a48049cb3
liuzhiyang <zhiyang1.liu@ximalaya.com>
2023-10-24 14:19:41 +0800

    feat: 去掉log

--- src/webpack/plugin/resolve.plugin/modTiktokenEntry.ts
+++ src/webpack/plugin/resolve.plugin/modTiktokenEntry.ts
@@ -4,7 +4,6 @@ class ModTiktokenEntry {
   apply(resolver: Resolver) {
     resolver.getHook('before-resolve-in-package').tapAsync('ModTiktoken', (request, resolveContext, callback) => {
       if (request.path && /@dqbd\/tiktoken/.test(request.path)) {
-        console.log(resolveContext);
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         request.descriptionFileData.exports['.'].node = request.descriptionFileData.exports['.'].default;
